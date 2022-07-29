000e3d9383c6038b7d4793319dfcd3a3f479b3a0
wenyxu <wenymedia@gmail.com>
2022-07-29 21:20:36 +0900

    feat(pkg/executor): add basic plans

---
+++ pkg/neo/executor/plans/abstract_plan.go
@@ -0,0 +1,55 @@
+package plans
+
+import "github.com/casbin-mesh/neo/pkg/primitive/bschema"
+
+type PlanType int
+
+const (
+	InsertPlanType PlanType = iota + 1
+	UpdatePlanType
+	SeqScanPlanType
+)
+
+type Plan interface {
+	GetType() PlanType
+}
+
+type AbstractPlan interface {
+	OutputSchema() bschema.Reader
+	HasChildren() bool
+	GetChildAt(idx int) AbstractPlan
+	GetChildren() []AbstractPlan
+	GetType() PlanType
+}
+
+type abstractPlan struct {
+	schema   bschema.Reader
+	children []AbstractPlan
+}
+
+func (a abstractPlan) OutputSchema() bschema.Reader {
+	return a.schema
+}
+
+func (a abstractPlan) HasChildren() bool {
+	return len(a.children) > 0
+}
+
+func (a abstractPlan) GetChildAt(idx int) AbstractPlan {
+	return a.children[idx]
+}
+
+func (a abstractPlan) GetChildren() []AbstractPlan {
+	return a.children
+}
+
+func (a abstractPlan) GetType() PlanType {
+	panic("unreachable")
+}
+
+func NewAbstractPlan(schema bschema.Reader, children []AbstractPlan) AbstractPlan {
+	return &abstractPlan{
+		schema:   schema,
+		children: children,
+	}
+}
