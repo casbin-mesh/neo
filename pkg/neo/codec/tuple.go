715d5c539296ba36a223bdb25349d9b655044401
wenyxu <wenymedia@gmail.com>
2022-07-31 01:51:11 +0900

    feat(neo/codec): add enocder

--- pkg/neo/codec/tuple.go
+++ pkg/neo/codec/tuple.go
@@ -11,3 +11,20 @@ func TupleRecordKey(tableId uint64, rid primitive.ObjectID) []byte {
 	buf = append(buf, rid[:]...)
 	return buf
 }
+
+func ParseTupleRecordKey(b []byte) (primitive.ObjectID, error) {
+	if len(b) != 19 {
+		return primitive.ObjectID{}, ErrInvalidKey
+	}
+	data := [8]byte{}
+	copy(data[:], b[11:])
+	return data, nil
+}
+
+func TupleRecordBegin(tableId uint64) []byte {
+	buf := make([]byte, 0, 11)
+	buf = append(buf, tablePrefix...)
+	buf = appendUint64(buf, tableId)
+	buf = append(buf, tupleRecordPrefix...)
+	return buf
+}
