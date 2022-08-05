5ad3a54d381409bf565f6fd9509c4da4fe57abae
noneback <chenlan_of_learn@126.com>
2022-08-05 16:26:14 +0900

    feat(neo/executor): introduce delete executor

---
+++ pkg/neo/executor/delete_executor_test.go
@@ -0,0 +1,56 @@
+package executor
+
+import (
+	"context"
+	"github.com/casbin-mesh/neo/pkg/neo/executor/plan"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestNewDeleteExecutor(t *testing.T) {
+	p := "./__test_tmp__/insert_exec"
+	mockDb := OpenMockDB(t, p)
+	defer func() {
+		mockDb.Close()
+		os.RemoveAll(p)
+	}()
+	setupMockDB(t, mockDb)
+
+	// insert tuples
+	sc := mockDb.NewTxnAt(4, true)
+	_, _, err := mockDb.InsertTuples(t, sc, 1, 1, mockDBDataSet)
+	err = sc.CommitTxn(context.TODO(), 5)
+	assert.Nil(t, err)
+
+	assert.Nil(t, mockDb.WaitForMark(context.TODO(), 5))
+
+	// delete tuples
+	sc = mockDb.NewTxnAt(6, true)
+	scan, err := NewSeqScanExecutor(sc, plan.NewSeqScanPlan(mockDBInfo1.TableInfo[0], nil, 1, 1))
+	assert.Nil(t, err)
+	exec, err := NewDeleteExecutor(sc, plan.NewDeletePlan(nil, 1, 1), scan)
+	assert.Nil(t, err)
+	result, ids, err := Execute(exec, context.TODO())
+	assert.Nil(t, err)
+	err = sc.CommitTxn(context.TODO(), 7)
+	assert.Nil(t, err)
+
+	assert.Nil(t, mockDb.WaitForMark(context.TODO(), 7))
+
+	// delete should not generate result
+	assert.Nil(t, result)
+	assert.Nil(t, ids)
+
+	// scan again
+	sc = mockDb.NewTxnAt(8, true)
+	scan, err = NewSeqScanExecutor(sc, plan.NewSeqScanPlan(mockDBInfo1.TableInfo[0], nil, 1, 1))
+	result, ids, err = Execute(scan, context.TODO())
+	err = sc.CommitTxn(context.TODO(), 9)
+	assert.Nil(t, err)
+
+	assert.Nil(t, mockDb.WaitForMark(context.TODO(), 9))
+
+	assert.Nil(t, result)
+	assert.Nil(t, ids)
+}
